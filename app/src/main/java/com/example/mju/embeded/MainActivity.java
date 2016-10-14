450117ad10c8236e057e257a5814823f95b9708a
usas0s <usas0s@naver.com>
2016-10-14 11:45:21 +0900

    메인에 어플리케이션 주석 달음 2016-10-14 11:44

--- app/src/main/java/com/example/mju/embeded/MainActivity.java
+++ app/src/main/java/com/example/mju/embeded/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mju.embeded;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//이 어플리케이션은 명지대학교 학생들이 모임을 쉽게 만들도록 도와주는 어플리케이션이다.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
