b0a4d2c2058c90b5f7613ad70a3267a955ca5385
김민혁 <qkqjwmd@gmail.com>
2016-10-17 12:02:57 +0900

    ADD : SearchResult Activity

---
+++ app/src/main/java/com/example/mju/embeded/SearchResult.java
@@ -0,0 +1,15 @@
+//Created at 17 Oct 16 by MH K
+
+package com.example.mju.embeded;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
