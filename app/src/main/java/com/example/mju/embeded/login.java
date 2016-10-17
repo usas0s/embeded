94caba9d1189f6851760ab0591064ea418c56631
Glisseo <waring1994@gmail.com>
2016-10-17 14:34:02 +0900

    login activity 수정

--- app/src/main/java/com/example/mju/embeded/login.java
+++ app/src/main/java/com/example/mju/embeded/login.java
@@ -37,7 +37,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class activity_login extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class login extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -273,7 +273,7 @@ public class activity_login extends AppCompatActivity implements LoaderCallbacks
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(activity_login.this,
+                new ArrayAdapter<>(login.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         mEmailView.setAdapter(adapter);
