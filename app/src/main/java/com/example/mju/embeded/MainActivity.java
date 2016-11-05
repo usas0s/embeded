6beb9344b111edcca2980c0e4fcf6e8b0161e5d0
dongbin <ehdqls357@naver.com>
2016-11-05 23:00:35 +0900

    main_activity navbar 구현

--- app/src/main/java/com/example/mju/embeded/MainActivity.java
+++ app/src/main/java/com/example/mju/embeded/MainActivity.java
@@ -1,20 +1,59 @@
 package com.example.mju.embeded;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 //이 어플리케이션은 명지대학교 학생들이 모임을 쉽게 만들도록 도와주는 어플리케이션이다.
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name , R.string.app_name);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     }
 
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     public void onClickDetail(View view){
         Intent detail_intent = new Intent(this,Details.class);
         startActivity(detail_intent);
@@ -23,5 +62,9 @@ public class MainActivity extends AppCompatActivity {
     public void onCilckLogin(View view) {
         Intent login_intent = new Intent(this, login.class);
         startActivity(login_intent);
+    }
+    public void onCilckSearch(View view) {
+        Intent search_intent = new Intent(this, login.class);
+        startActivity(search_intent);
     }
 }
