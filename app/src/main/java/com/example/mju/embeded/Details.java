f670fc0502cf2a1670766191fc98bf6c5a6a2492
usas0s <usas0s@naver.com>
2016-11-08 19:15:35 +0900

    2016-11-08 modified img file name again

--- app/src/main/java/com/example/mju/embeded/Details.java
+++ app/src/main/java/com/example/mju/embeded/Details.java
@@ -1,5 +1,8 @@
 package com.example.mju.embeded;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,9 +24,28 @@ public class Details extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "참가 신청하기", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+//        ImageView img = (ImageView)findViewById(R.id.imageView1);
+//        Resources res = getResources();
+//
+//        Bitmap bitmap = BitmapFactory.decodeResource(res, android.R.drawable.btn_star_big_on);
+//        Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap, 200, 90, false);
+//        img.setImageBitmap(bitmap2);
+
+//        BitmapFactory.Options opts = new BitmapFactory.Options();
+//        opts.inPreferredConfig = Bitmap.Config.ARGB_8888;
+//        Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+//                R.drawable.img1, opts);
+//        ImageView view = (ImageView) findViewById(R.id.imageView1);
+//        view.setImageBitmap(bmp);
+
+//        <ImageView
+//        android:layout_width="match_parent"
+//        android:layout_height="match_parent"
+//        android:id="@+id/imageView1"/>
     }
 }
