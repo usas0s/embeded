f93a52d9e21eaf6d99c80614290b0f92c4ffb4ee
usas0s <usas0s@naver.com>
2016-11-16 23:04:55 +0900

    161116 finished detail act / added GPS / added apply act.

--- app/src/main/java/com/example/mju/embeded/Details.java
+++ app/src/main/java/com/example/mju/embeded/Details.java
@@ -1,8 +1,10 @@
 package com.example.mju.embeded;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Details extends AppCompatActivity {
 
@@ -22,11 +25,34 @@ public class Details extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            // floating 버튼 터치 시 참가 신청 페이지 열림.
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "참가 신청하기", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "참가 신청하기", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Snackbar snackbar;
+                snackbar = Snackbar.make(view, "참가 신청하기", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener()
+                        {
+                            // 참가 신청 버튼 터치 시 할일
+                            @Override
+                            public void onClick(View view)
+                            {
+                                Toast.makeText(getApplicationContext(),"moving to Apply activity",Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getApplicationContext(),Apply.class);
+                                startActivity(intent);
+                            }
+                        });
+                View snackView = snackbar.getView();
+                snackView.setBackgroundColor(Color.parseColor("#0000FF"));
+                snackbar.show();
             }
         });
+    }
+
+    public void showMap(View view)
+    {
+        Intent intent = new Intent(this,Maps.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mju/embeded/Maps.java
@@ -0,0 +1,67 @@
+package com.example.mju.embeded;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Maps extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+
+        // Add a marker in Sydney and move the camera
+        LatLng spot = new LatLng(37.398291,126.963327);
+        mMap.addMarker(new MarkerOptions().position(spot).title("스팟").snippet("spot")).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(spot));
+
+        // zoom in properly to spot
+        mMap.animateCamera(zoom);
+
+        // 마커클릭 이벤트 처리
+        // GoogleMap 에 마커클릭 이벤트 설정 가능.
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                // 마커 클릭시 호출되는 콜백 메서드
+                Toast.makeText(getApplicationContext(),
+                        marker.getTitle() + " is hot place!"
+                        , Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+}
