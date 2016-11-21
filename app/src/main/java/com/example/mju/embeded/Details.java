8ade04d11521010fdc7d117dc5a577cf0148cc3f
dongbin <ehdqls357@naver.com>
2016-11-21 18:38:02 +0900

    MainActivity - Navigation Bar/Tabs/ViewPager 추가

--- app/src/main/java/com/example/mju/embeded/Details.java
+++ app/src/main/java/com/example/mju/embeded/Details.java
@@ -1,9 +1,6 @@
 package com.example.mju.embeded;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,7 +8,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Details extends AppCompatActivity {
@@ -28,6 +24,8 @@ public class Details extends AppCompatActivity {
             // floating 버튼 터치 시 참가 신청 페이지 열림.
             @Override
             public void onClick(View view) {
+//                Snackbar.make(view, "참가 신청하기", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
                 Snackbar snackbar;
                 snackbar = Snackbar.make(view, "참가 신청하기", Snackbar.LENGTH_LONG)
                         .setAction("Action", new View.OnClickListener()
@@ -36,13 +34,13 @@ public class Details extends AppCompatActivity {
                             @Override
                             public void onClick(View view)
                             {
-                                Toast.makeText(getApplicationContext(),"참가 신청 페이지로 이동합니다.",Toast.LENGTH_LONG).show();
+                                Toast.makeText(getApplicationContext(),"moving to Apply activity",Toast.LENGTH_LONG).show();
                                 Intent intent = new Intent(getApplicationContext(),Apply.class);
                                 startActivity(intent);
                             }
                         });
                 View snackView = snackbar.getView();
-                snackView.setBackgroundColor(Color.parseColor("#3498DB"));
+                snackView.setBackgroundColor(Color.parseColor("#0000FF"));
                 snackbar.show();
             }
         });
--- app/src/main/java/com/example/mju/embeded/MainActivity.java
+++ app/src/main/java/com/example/mju/embeded/MainActivity.java
@@ -1,59 +1,92 @@
 package com.example.mju.embeded;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
-//이 어플리케이션은 명지대학교 학생들이 모임을 쉽게 만들도록 도와주는 어플리케이션이다.
+/**
+ * Copyright (C) 컴퓨터공학과 60112320 김동빈
+ */
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name , R.string.app_name);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawerLayout);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-    }
-
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_navView);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_viewPager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ListFragment(), "Category 1");
+        adapter.addFragment(new ListFragment(), "Category 2");
+        adapter.addFragment(new ListFragment(), "Category 3");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
     public void onClickDetail(View view){
         Intent detail_intent = new Intent(this,Details.class);
         startActivity(detail_intent);
@@ -67,4 +100,5 @@ public class MainActivity extends AppCompatActivity
         Intent search_intent = new Intent(this, SearchResult.class);
         startActivity(search_intent);
     }
+
 }
