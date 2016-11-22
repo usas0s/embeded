202dc713e3c87f522bb57f734de78972eec5e289
usas0s <usas0s@naver.com>
2016-11-22 18:24:57 +0900

    161122 modulized detail act.

--- app/src/main/java/com/example/mju/embeded/Details.java
+++ app/src/main/java/com/example/mju/embeded/Details.java
@@ -1,18 +1,25 @@
 package com.example.mju.embeded;
 
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Details extends AppCompatActivity {
 
@@ -46,6 +53,44 @@ public class Details extends AppCompatActivity {
                 snackbar.show();
             }
         });
+
+        // 썸네일 셋팅
+        ImageView iv_thumbnail = (ImageView)findViewById(R.id.detail_thumbnail);
+//        String path = DB.image_path;
+        String path = "acube0";
+        int resID = getResources().getIdentifier(path,"drawable",getPackageName());
+        iv_thumbnail.setImageResource(resID);
+//        -> 한줄로 수정하면 iv_thumbnail.setImageResource(getResources().getIdentifier(DB.image_path,"drawable",getPackageName()));
+
+//        iv_thumbnail.setImageResource(R.drawable.acube0); // DB's image_path 받는걸로 수정
+
+        // 모임 기간 셋팅
+        TextView tv_period = (TextView) findViewById(R.id.detail_period);
+        tv_period.setText("DB.period");
+
+        // 모임 장소 셋팅
+        TextView tv_place = (TextView) findViewById(R.id.detail_place);
+        tv_place.setText("DB.place");
+
+        // 최대 인원 셋팅
+        TextView tv_limit = (TextView) findViewById(R.id.detail_limit);
+        tv_limit.setText("총 DB.limit명 | ");
+
+        // 현재 인원 셋팅
+        TextView tv_current = (TextView) findViewById(R.id.detail_current);
+        tv_current.setText("DB.current명 신청 가능");
+
+        // 상세 설명 셋팅
+        TextView tv_description = (TextView) findViewById(R.id.detail_description);
+        tv_description.setText("DB.description");
+
+        // 설명 이미지 셋팅
+        ImageView iv_image = (ImageView)findViewById(R.id.detail_image);
+//        String path = DB.image_path;
+        String path2 = "in";
+        int resID2 = getResources().getIdentifier(path2,"drawable",getPackageName());
+        iv_image.setImageResource(resID2);
+//        -> 한줄로 수정하면 iv_thumbnail.setImageResource(getResources().getIdentifier(DB.image_path,"drawable",getPackageName()));
     }
 
     public void showMap(View view)
@@ -53,4 +98,39 @@ public class Details extends AppCompatActivity {
         Intent intent = new Intent(this,Maps.class);
         startActivity(intent);
     }
+//
+//    private void sendShare() {
+//        Intent intent = new Intent(Intent.ACTION_SEND);
+//        intent.setType("image/*");
+//
+//        List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(intent, 0);
+//        if (resInfo.isEmpty()) {
+//            return;
+//        }
+//
+//        List<Intent> shareIntentList = new ArrayList<Intent>();
+//
+//        for (ResolveInfo info : resInfo) {
+//            Intent shareIntent = (Intent) intent.clone();
+//
+//            if (info.activityInfo.packageName.toLowerCase().equals("com.facebook.katana")) {
+//                //facebook
+//                shareIntent.setType("text/plain");
+//                shareIntent.putExtra(Intent.EXTRA_TEXT, "http://www.google.com");
+////                shareIntent.setType("image/jpg");
+////                shareIntent.putExtra(Intent.EXTRA_STREAM,  Uri.parse("file:///"+mImagePath));
+//            } else {
+//                shareIntent.setType("image/*");
+//                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "제목");
+//                shareIntent.putExtra(Intent.EXTRA_TEXT, "구글 http://www.google.com #");
+//                shareIntent.putExtra(Intent.EXTRA_STREAM,  Uri.parse("file:///"));
+//            }
+//            shareIntent.setPackage(info.activityInfo.packageName);
+//            shareIntentList.add(shareIntent);
+//        }
+//
+//        Intent chooserIntent = Intent.createChooser(shareIntentList.remove(0), "select");
+//        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, shareIntentList.toArray(new Parcelable[]{}));
+//        startActivity(chooserIntent);
+//    }
 }
--- app/src/main/java/com/example/mju/embeded/Maps.java
+++ app/src/main/java/com/example/mju/embeded/Maps.java
@@ -1,5 +1,7 @@
 package com.example.mju.embeded;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +12,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,7 +31,6 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -43,13 +45,45 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
 
-        // Add a marker in Sydney and move the camera
-        LatLng spot = new LatLng(37.398291,126.963327);
+        // Add a marker in spot and move the camera
+        LatLng spot = new LatLng(37.398291, 126.963327);
         mMap.addMarker(new MarkerOptions().position(spot).title("스팟").snippet("spot")).showInfoWindow();
         mMap.moveCamera(CameraUpdateFactory.newLatLng(spot));
 
+        // UI settings
+        UiSettings mapSettings = mMap.getUiSettings();
+        mapSettings.setZoomControlsEnabled(true);
+        mapSettings.setCompassEnabled(true);
+//        mapSettings.setIndoorLevelPickerEnabled(true);
+
+        // my location
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            mMap.setMyLocationEnabled(true);
+//            mapSettings.setMyLocationButtonEnabled(true);
+//            return;
+//        }
+
         // zoom in properly to spot
         mMap.animateCamera(zoom);
+
+        // map click
+//        mMap.setOnMapClickListener();
+//        mMap.setOnMapLongClickListener(this);
+//        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
+//            @Override
+//            public void OnMapLongClick(LatLng spot)
+//            {
+//                Toast.makeText(getApplicationContext(),"Click : " + String.valueOf(spot.latitude)
+//                        + " / " + String.valueOf(spot.longitude), Toast.LENGTH_LONG);
+//            }
+//        });
 
         // 마커클릭 이벤트 처리
         // GoogleMap 에 마커클릭 이벤트 설정 가능.
@@ -64,4 +98,16 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
             }
         });
     }
+
+//    public void onMapClick(LatLng spot)
+//    {
+//        Toast.makeText(this,"Click : " + String.valueOf(spot.latitude) + " / "
+//                + String.valueOf(spot.longitude), Toast.LENGTH_LONG);
+//    }
+//
+//    public void onMapLongClick(LatLng spot)
+//    {
+//        Toast.makeText(this,"LongClick : " + String.valueOf(spot.latitude) + " / "
+//                + String.valueOf(spot.longitude), Toast.LENGTH_LONG);
+//    }
 }
