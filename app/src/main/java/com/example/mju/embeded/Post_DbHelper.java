9db6bb260c709105f0c61fc2c1be5cf3e8ea3c16
김민혁 <qkqjwmd@gmail.com>
2016-11-25 20:31:32 +0900

    Add DB and Modified some funcs

---
+++ app/src/main/java/com/example/mju/embeded/Post_DbHelper.java
@@ -0,0 +1,34 @@
+package com.example.mju.embeded;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Post_DbHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+
+    public Post_DbHelper(Context context)
+    {
+        super(context, Post_Contract.DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL(Post_Contract.SQL_PRAGMA_ON);
+        db.execSQL(Post_Contract.SQL_DELETE_TABLE);
+        db.execSQL(Post_Contract.SQL_CREATE_TABLE);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        //db.execSQL(Post_Contract.SQL_DELETE_TABLE);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL(Post_Contract.SQL_DELETE_TABLE);
+    }
+}
