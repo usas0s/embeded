87b087ee7af706e5f9c188f61163d225fb0da671
dongbin <ehdqls357@naver.com>
2016-11-28 15:30:58 +0900

    Main Activity Gridview 적용

---
+++ app/src/main/java/com/example/mju/embeded/Main_ViewpagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.mju.embeded;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Bini2 on 2016-11-25.
+ */
+
+public class Main_ViewpagerAdapter extends FragmentPagerAdapter {
+    public Main_ViewpagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return Main_PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+            case 2:
+                return "SECTION 3";
+        }
+        return null;
+    }
+}
