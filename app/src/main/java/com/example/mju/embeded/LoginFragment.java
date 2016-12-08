6f038e667b9b78cb7cb50397cd551187b90a981f
Glisseo <waring1994@gmail.com>
2016-12-08 15:27:35 +0900

    nav 로그인바 복구

--- app/src/main/java/com/example/mju/embeded/LoginFragment.java
+++ app/src/main/java/com/example/mju/embeded/LoginFragment.java
@@ -11,14 +11,9 @@ public class LoginFragment extends Fragment {
     private View v;
 
     @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if(Login.Login_State) v = inflater.inflate(R.layout.fragment_intro2, container, true);
-        else v = inflater.inflate(R.layout.fragment_intro1, container, true);
+        if(Login.Login_State) v = inflater.inflate(R.layout.fragment_login2, container, true);
+        else v = inflater.inflate(R.layout.fragment_login1, container, true);
 
         return v;
     }
